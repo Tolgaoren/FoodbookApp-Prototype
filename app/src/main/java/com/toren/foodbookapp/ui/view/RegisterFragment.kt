bd83c6ad6f369aff548e602feb4c293260737932
mertgundoganx <mert.gundogan@outlook.com>
2021-10-10 02:49:07 +0300

    register user control fixed.

--- app/src/main/java/com/toren/foodbookapp/ui/view/RegisterFragment.kt
+++ app/src/main/java/com/toren/foodbookapp/ui/view/RegisterFragment.kt
@@ -1,13 +1,12 @@
 package com.toren.foodbookapp.ui.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.isEmpty
-import androidx.core.view.size
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.toren.foodbookapp.databinding.RegisterFragmentBinding
@@ -49,22 +48,32 @@ class RegisterFragment : Fragment() {
             if (inputName.editText!!.text.toString().isEmpty() || inputName.editText!!.text.length < 3) {
                 inputName.error = "Geçerli bir isim giriniz."
                 control = false
+            } else {
+                inputName.error = null
             }
             if (inputSurname.editText!!.text.toString().isEmpty() || inputSurname.editText!!.text.length < 3) {
                 inputSurname.error = "Geçerli bir isim giriniz."
                 control = false
+            } else {
+                inputSurname.error = null
             }
             if (inputMail.editText!!.text.isEmpty()) {
                 inputMail.error = "Geçerli bir email adresi giriniz."
                 control = false
+            } else {
+                inputMail.error = null
             }
             if (inputPassword.editText!!.text.isEmpty() || inputPassword.editText!!.text.length < 6) {
                 inputPassword.error = "Minimum 6 karakterli bir parola belirleyiniz."
                 control = false
+            } else {
+                inputPassword.error = null
             }
-            if (inputPasswordControl.editText!!.text.isEmpty() || inputPasswordControl.editText!!.text.length < 6) {
-                inputPasswordControl.error = "Minimum 6 karakterli bir parola belirleyiniz."
+            if (inputPasswordControl.editText!!.text.toString() != inputPassword.editText!!.text.toString()) {
+                inputPasswordControl.error = "Lütfen parolayı aynı giriniz."
                 control = false
+            } else {
+                inputPasswordControl.error = null
             }
         }
 
