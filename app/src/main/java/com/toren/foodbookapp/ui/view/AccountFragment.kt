fb7620acf55ac293416fbd4ae692c64803d19731
mertgundoganx <mert.gundogan@outlook.com>
2021-10-10 00:53:43 +0300

    fixed

--- app/src/main/java/com/toren/foodbookapp/ui/view/AccountFragment.kt
+++ app/src/main/java/com/toren/foodbookapp/ui/view/AccountFragment.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import com.toren.foodbookapp.ui.viewmodel.AccountViewModel
 import com.toren.foodbookapp.R
 
