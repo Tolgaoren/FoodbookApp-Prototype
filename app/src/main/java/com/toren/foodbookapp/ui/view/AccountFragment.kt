931b7801fbeba86e083eff12dc3edb8107c5151e
orent <orentolga@hotmail.com>
2021-10-10 21:37:13 +0300

    Account update

--- app/src/main/java/com/toren/foodbookapp/ui/view/AccountFragment.kt
+++ app/src/main/java/com/toren/foodbookapp/ui/view/AccountFragment.kt
@@ -1,24 +1,53 @@
 package com.toren.foodbookapp.ui.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.toren.foodbookapp.ui.viewmodel.AccountViewModel
 import com.toren.foodbookapp.R
+import com.toren.foodbookapp.databinding.AccountFragmentBinding
+import com.toren.foodbookapp.databinding.LoginFragmentBinding
 
 class AccountFragment : Fragment() {
 
     private val viewModel: AccountViewModel by viewModels()
+    private lateinit var binding: AccountFragmentBinding
+    private lateinit var auth: FirebaseAuth
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.account_fragment, container, false)
+    ): View {
+        binding = AccountFragmentBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        binding.signOutButton.setOnClickListener(){
+            actionToLogin()
+        }
+    }
+
+    private fun actionToLogin() {
+        auth.signOut()
+        val action = AccountFragmentDirections.actionAccountFragmentToLoginFragment()
+        findNavController().navigate(action)
+    }
+
 
 
 }
