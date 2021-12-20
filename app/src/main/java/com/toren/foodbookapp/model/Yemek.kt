ddd5b716a840db45dffe530bd7b792189b4dd9eb
Mert Gundogan <mert.gundogan@outlook.com>
2021-12-20 23:22:59 +0300

    add food fragment

--- app/src/main/java/com/toren/foodbookapp/model/Yemek.kt
+++ app/src/main/java/com/toren/foodbookapp/model/Yemek.kt
@@ -3,7 +3,7 @@ package com.toren.foodbookapp.model
 data class Yemek(
     val yemekIsmi: String,
     val malzemeler: ArrayList<String>,
-    val malzemelerDetayli: ArrayList<String>,
+    val malzemelerDetayli: ArrayList<Material>,
     val hazirlanis: String,
     val kategori: String,
     val imgUrl: String,
