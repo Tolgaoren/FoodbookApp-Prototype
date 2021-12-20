b149232460580276c1646756cdc19fef57ecd85e
Mert Gundogan <mert.gundogan@outlook.com>
2021-12-21 02:34:17 +0300

    realtime database to firestore

--- app/src/main/java/com/toren/foodbookapp/ui/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/toren/foodbookapp/ui/viewmodel/RegisterViewModel.kt
@@ -2,8 +2,7 @@ package com.toren.foodbookapp.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.toren.foodbookapp.model.Users
 import kotlinx.coroutines.Dispatchers
@@ -11,11 +10,11 @@ import kotlinx.coroutines.launch
 
 class RegisterViewModel : ViewModel() {
 
-    private val database: DatabaseReference = Firebase.database.reference
+    private val db = Firebase.firestore
 
     fun saveNewUser(user: Users) {
         viewModelScope.launch(Dispatchers.IO) {
-            database.child("users").child(user.uuid).setValue(user)
+            db.collection("users").document(user.uuid).set(user)
         }
     }
 
