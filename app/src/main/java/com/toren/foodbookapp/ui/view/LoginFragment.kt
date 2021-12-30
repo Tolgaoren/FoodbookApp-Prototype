7d4cb79b86503969efdcaec80204be296535e6c7
Mert Gundogan <mert.gundogan@outlook.com>
2021-12-30 04:40:50 +0300

    login sorunu düzeltildi.

--- app/src/main/java/com/toren/foodbookapp/ui/view/LoginFragment.kt
+++ app/src/main/java/com/toren/foodbookapp/ui/view/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.toren.foodbookapp.ui.viewmodel.LoginViewModel
@@ -44,7 +45,11 @@ class LoginFragment : Fragment() {
     private fun loginAccount() {
         viewModel.control.observe(viewLifecycleOwner, {
             it?.let {
-                actionToHome()
+                if (it) {
+                    actionToHome()
+                } else {
+                    Toast.makeText(this.context,"Kullanıcı bilgileri hatalı",Toast.LENGTH_SHORT).show()
+                }
             }
         })
     }
